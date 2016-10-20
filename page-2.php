f4a8f1fb48d56cff24143ba94782e310c61eda01
Petar <petar.bosnjak@yahoo.com>
2016-10-20 16:55:18 +0200

    drugi

---
+++ page-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>PHP akademija</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>              
+        <h1>Naslov</h1>
+        <img src="static/rickrolled-card.jpg">
+              
+    </body>
+    <footer>
+         <p><?php echo date(' d.m.Y ') ?></p>
+    </footer>
+</html>
